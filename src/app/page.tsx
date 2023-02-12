4cb4fb2bcf3193f5de424b78e51ac0b904853646
82109@DESKTOP-CP7KARN <81937738+sktjgudals@users.noreply.github.com>
2023-02-13 00:20:39 +0900

    [Style] 메인 페이지 스타일 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,19 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          ></a>
         </div>
       </div>
 
@@ -40,9 +26,7 @@ export default function Home() {
           height={37}
           priority
         />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
+        <div className={styles.thirteen}>Loading...</div>
       </div>
 
       <div className={styles.grid}>
@@ -87,5 +71,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
