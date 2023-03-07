80c4effd954373f581aef2e77f2dd05e713a1486
sktjgudals <sktjgudals@naver.com>
2023-03-07 14:04:49 +0900

    [Feat] 아토믹 패턴 기본 컴포넌트 추가 및 페이지 인덱스 변경

---
+++ src/components/atoms/Label.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+import styled from "styled-components";
+
+interface Props {
+  text: string;
+  fontSize: number;
+}
+
+interface StyledLabelProps {
+  fontSize: number;
+}
+
+const Label: FC<Props> = ({ text, fontSize }) => {
+  return <StyledLabel fontSize={fontSize}>{text}</StyledLabel>;
+};
+
+export default Label;
+
+const StyledLabel = styled.label<StyledLabelProps>`
+  font-size: ${(props) => props.fontSize};
+`;
