b94118889753ab3ff8d5464565a83ca9c15ffecf
sktjgudals <sktjgudals@naver.com>
2023-03-28 23:45:12 +0900

    [MISC] search 수정

--- src/components/molecules/Search.tsx
+++ src/components/molecules/Search.tsx
@@ -1,43 +1,56 @@
-import { FC } from "react";
+import { FC, useRef } from "react";
 import styled from "styled-components";
+import useInput from "@/hooks/useInput";
 
 import styles from "../../../assets/styles/molecules/Search.module.scss";
 import { Input } from "../atoms/Input";
 import { Button } from "../atoms/Button";
 import SearchIcon from "../atoms/icons/SearchIcon";
-import useInput from "@/hooks/useInput";
 
 const Search: FC = () => {
   const [userFormInput, onChangeForm] = useInput("");
-  const onClickHandle = () => {};
+  const searchInput: any = useRef();
 
+  const a = (e: React.FormEvent) => {
+    console.info(userFormInput.toString().length === 0);
+    if (userFormInput.toString().length === 0) {
+      searchInput.current = null;
+      e.preventDefault();
+    }
+  };
   return (
-    <form action={`/search`} role="search" method="GET">
-      <div className={styles.top_nav_search_container}>
-        <div className={styles.search_box}>
-          <div
-            className={styles.input_box}
-            data-target="tray-search-input"
-            aria-haspopup="grid"
-          >
-            <div className={styles.search_input}>
-              <SearchInput
-                placeholder={"검색"}
-                value={userFormInput}
-                onChange={onChangeForm}
-              />
-              <input type="hidden" name="keyword" value={userFormInput} />
-              <input type="hidden" name="sort" value={"like_count"} />
-              <input type="hidden" name="order" value={"ascending"} />
-              <input type="hidden" name="page" value={"1"} />
-            </div>
+    <form
+      action={`/search`}
+      role="search"
+      method="GET"
+      className={styles.top_nav_search_container}
+      onSubmit={a}
+    >
+      <div className={styles.search_box}>
+        <div
+          className={styles.input_box}
+          data-target="tray-search-input"
+          aria-haspopup="grid"
+        >
+          <div className={styles.search_input}>
+            <SearchInput
+              className={styles.cancel}
+              placeholder={"검색"}
+              value={userFormInput}
+              onChange={onChangeForm}
+              ref={searchInput}
+            />
+            <input type="hidden" name="keyword" value={userFormInput} />
+            <input type="hidden" name="sort" value={"like_count"} />
+            <input type="hidden" name="order" value={"ascending"} />
+            <input type="hidden" name="page" value={"1"} />
           </div>
         </div>
-        <div className={styles.search_icon}>
-          <SearchButton width={40} height={39}>
-            <SearchIcon />
-          </SearchButton>
-        </div>
+      </div>
+      <div className={styles.search_icon}>
+        <SearchButton width={40} height={39}>
+          <SearchIcon />
+        </SearchButton>
       </div>
     </form>
   );
