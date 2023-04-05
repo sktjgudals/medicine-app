f2fdefce684ca5340c7338f6bfa64fea7f4d6adf
sktjgudals <sktjgudals@naver.com>
2023-04-05 13:55:09 +0900

    [MISC] search 아이콘 수정

--- src/components/atoms/icons/SearchIcon.tsx
+++ src/components/atoms/icons/SearchIcon.tsx
@@ -1,18 +1,19 @@
 import { FC } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { BasicIconProps } from "@/types/context/icon/basic";
 
-interface Props {
-  width: number;
-  height: number;
+interface Props extends BasicIconProps {
+  color?: string;
 }
-const SearchIcon: FC<Props> = ({ width, height }) => {
+
+const SearchIcon: FC<Props> = ({ width, height, color }) => {
   return (
     <FontAwesomeIcon
       icon={faMagnifyingGlass}
       width={width}
       height={height}
-      // color={"green"}
+      color={color}
     />
   );
 };
