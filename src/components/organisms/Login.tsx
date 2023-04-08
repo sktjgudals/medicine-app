aa686e23fb5a3722b5a0bc3ac9c18a1ca281fb3e
sktjgudals <sktjgudals@naver.com>
2023-04-08 14:41:51 +0900

    [Feat] modal

---
+++ src/components/organisms/Login.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { Modal, ModalWrapper } from "../atoms/Modal";
+
+const Login: FC = () => {
+  return (
+    <>
+      <ModalWrapper>{/* <Modal width={}/> */}</ModalWrapper>
+    </>
+  );
+};
+
+export default Login;
