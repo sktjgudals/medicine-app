117ed2d0b2aec465f7adf34035b7dd699e011ba1
sktjgudals <sktjgudals@naver.com>
2023-04-10 22:33:38 +0900

    [MISC] children 필수

--- src/components/atoms/Portal.tsx
+++ src/components/atoms/Portal.tsx
@@ -2,7 +2,7 @@ import { FC, ReactNode } from "react";
 import { createPortal } from "react-dom";
 
 interface PortalProps {
-  children?: ReactNode;
+  children: ReactNode;
   selector: string;
 }
 
