05baabcb199aa81c9bef301fe83b3eb2bb81b7b2
sktjgudals <sktjgudals@naver.com>
2023-04-11 13:00:42 +0900

    [MISC] Button 기본 추가

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -8,6 +8,8 @@ interface StyledButtonProps {
 const Button = styled.button<StyledButtonProps>`
   width: ${(props) => props.width}px;
   height: ${(props) => props.height}px;
+  border: none;
+  cursor: pointer;
 `;
 
 export { Button };
