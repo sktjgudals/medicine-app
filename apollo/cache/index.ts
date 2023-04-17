abe12cda9da4b8ed9e4b62c2ff964825cdffc13f
sktjgudals <sktjgudals@naver.com>
2023-04-17 20:17:40 +0900

    [Feat]  signup 훅 추가 및 상태관리 위한 스테이트 추가

--- apollo/cache/index.ts
+++ apollo/cache/index.ts
@@ -2,6 +2,8 @@ import { makeVar } from "@apollo/client";
 
 const modalState = makeVar<boolean>(false);
 
+const signUpModalState = makeVar<boolean>(false);
+
 const dropdownState = makeVar<boolean>(false);
 
-export { modalState, dropdownState };
+export { modalState, dropdownState, signUpModalState };
