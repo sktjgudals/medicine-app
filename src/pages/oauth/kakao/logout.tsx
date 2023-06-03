c56b55ed1339b121e3761ce05ff0b25fe09800ca
sktjgudals <sktjgudals@naver.com>
2023-06-04 04:04:53 +0900

    [test]test

--- src/pages/oauth/kakao/logout.tsx
+++ src/pages/oauth/kakao/logout.tsx
@@ -3,6 +3,7 @@ import { FC, useEffect } from "react";
 
 const logout: FC = () => {
   const router = useRouter();
+  console.log("hi");
   useEffect(() => {
     const redirect_uri = localStorage.getItem("redirect_uri");
     if (redirect_uri) {
