463cf2a486d0ee22db3ca8c4d31bc8697c8dff6b
sktjgudals <sktjgudals@naver.com>
2023-06-07 09:33:46 +0900

    Revert "[MISC] oauth update"
    This reverts commit f632a3f8b53f8073986c595598410d993701dc0b.

--- src/pages/oauth/kakao/logout.tsx
+++ src/pages/oauth/kakao/logout.tsx
@@ -1,26 +1,19 @@
 import Title from "@/components/atoms/Title";
 import NotFoundPage from "@/pages/404";
 import { useRouter } from "next/router";
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect } from "react";
 
 const logout: FC = () => {
-  const [loading, setLoading] = useState<boolean>(false);
   const router = useRouter();
   useEffect(() => {
-    setLoading(true);
     const redirect_uri = localStorage.getItem("redirect_uri");
     if (redirect_uri) {
       router.push(redirect_uri);
-      router.reload();
       localStorage.removeItem("redirect_uri");
-      setLoading(false);
     } else {
       router.push("/");
-      router.reload();
-      setLoading(false);
     }
   }, []);
-  if (loading) return <></>;
   return (
     <div>
       <Title title={"카카오 로그아웃 - 약정"} content={"약을 찾아주는 요정"} />
