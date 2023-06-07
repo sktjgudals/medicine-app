f632a3f8b53f8073986c595598410d993701dc0b
sktjgudals <sktjgudals@naver.com>
2023-06-07 09:28:11 +0900

    [MISC] oauth update

--- src/pages/oauth/kakao.tsx
+++ src/pages/oauth/kakao.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
 import { oauthKakaoUserCode } from "@/utils/func/oauth";
@@ -13,20 +13,30 @@ interface Props {
 }
 
 const Kakao: FC<Props> = ({ access_token, refresh_token }) => {
+  const [loading, setLoading] = useState<boolean>(false);
   const router = useRouter();
+
   useEffect(() => {
+    setLoading(true);
     if (access_token && refresh_token) {
       tokenSet(access_token, refresh_token);
       const redirect_uri = localStorage.getItem("redirect_uri");
       if (redirect_uri) {
         router.push(redirect_uri as string);
+        router.reload();
         localStorage.removeItem("redirect_uri");
+        setLoading(false);
       } else {
         router.push("/");
+        router.reload();
+        setLoading(false);
       }
+    } else {
+      setLoading(false);
     }
   }, [access_token, refresh_token]);
 
+  if (loading) return <></>;
   return (
     <>
       <Title title={"카카오 로그인 - 약정"} content={"약을 찾아주는 요정"} />
